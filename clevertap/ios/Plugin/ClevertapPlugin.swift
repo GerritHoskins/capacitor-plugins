c88bb3d95f7ecb8d63b18b01aa31bf45e56a1825
GerritHoskins <gerrit.hoskins@gmail.com>
2022-09-19 13:27:15 +0200

    update clevertap

--- clevertap/ios/Plugin/ClevertapPlugin.swift
+++ clevertap/ios/Plugin/ClevertapPlugin.swift
@@ -7,8 +7,26 @@ import CleverTapSDK
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(ClevertapPlugin)
-public class ClevertapPlugin: CAPPlugin {
+public class ClevertapPlugin: CAPPlugin, UNUserNotificationCenterDelegate, CleverTapPushNotificationDelegate {
     override public func load() {
-        NSLog("clevertap plugin loaded")
+        self.registerForPush()
+
+        CleverTap.autoIntegrate()
+        CleverTap.setDebugLevel(CleverTapLogLevel.debug.rawValue)
+        NSLog("CleverTap Plugin loaded")
+        CleverTap.sharedInstance()?.enableDeviceNetworkInfoReporting(true)
+
+        super.load()
+    }
+
+    func registerForPush() {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .badge, .alert], completionHandler: {granted, _ in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        })
     }
 }
