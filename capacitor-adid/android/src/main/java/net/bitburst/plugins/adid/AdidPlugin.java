67d573e161148ac95b539bd743635719e571d8f7
GerritHoskins <gerrit.hoskins@gmail.com>
2022-09-21 13:35:48 +0200

    bump adid plugin package version

--- capacitor-adid/android/src/main/java/net/bitburst/plugins/adid/AdidPlugin.java
+++ capacitor-adid/android/src/main/java/net/bitburst/plugins/adid/AdidPlugin.java
@@ -5,18 +5,26 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
 @CapacitorPlugin(name = "Adid")
 public class AdidPlugin extends Plugin {
-    private final Adid adid = new Adid(getContext());
+    private Adid adid;
+
+    @Override
+    public void load() {
+        adid  = new Adid(getContext());
+        super.load();
+    }
 
     @PluginMethod
     public void getId(PluginCall call) {
         call.setKeepAlive(true);
         JSObject ret = new JSObject();
         try {
-            ret.put("id", adid.getAdIdInfo().getId());
-            ret.put("limitedAdTracking", adid.getAdIdInfo().isLimitAdTrackingEnabled());
+            AdvertisingIdClient.Info adInfo = adid.getAdIdInfo();
+            ret.put("id", adInfo.getId());
+            ret.put("limitedAdTracking", adInfo.isLimitAdTrackingEnabled());
         } catch (Exception e) {
             call.reject(Adid.LOG_TAG, "error getting advertising id.", e);
         }
