0feb4cd48b9331cc096507a730569fbf6a909bdd
GerritHoskins <gerrit.hoskins@gmail.com>
2022-09-22 11:43:16 +0200

    update mparticle package version

--- mparticle/src/index.ts
+++ mparticle/src/index.ts
@@ -1,17 +1,10 @@
 import { registerPlugin } from '@capacitor/core';
 
-import type { DefaultEvent, MparticlePlugin, mParticle } from './definitions';
+import type { MparticlePlugin } from './definitions';
 
 const Mparticle = registerPlugin<MparticlePlugin>('Mparticle', {
   web: () => import('./web').then(m => new m.MparticleWeb()),
 });
 
-declare const _default: {
-  getInstance<Events = DefaultEvent, ScreenEvents = DefaultEvent>(): mParticle<
-    Events,
-    ScreenEvents
-  >;
-};
-
 export * from './definitions';
-export default { Mparticle, _default };
+export default { Mparticle };
