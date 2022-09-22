740a61e2605304fe67ed4dadd2fd956f7b3d575e
GerritHoskins <gerrit.hoskins@gmail.com>
2022-09-22 09:30:56 +0200

    update mparticle package version

--- mparticle/src/index.ts
+++ mparticle/src/index.ts
@@ -1,14 +1,10 @@
 import { registerPlugin } from '@capacitor/core';
 
 import type { MparticlePlugin } from './definitions';
-import type { MpTracking } from './plugin';
 
 const Mparticle = registerPlugin<MparticlePlugin>('Mparticle', {
   web: () => import('./web').then(m => new m.MparticleWeb()),
 });
 
-const Tracking = registerPlugin<typeof MpTracking>('Tracking', {
-  web: () => import('./plugin').then(m => new m.MpTracking()),
-});
 export * from './definitions';
-export { Mparticle, Tracking };
+export { Mparticle };
